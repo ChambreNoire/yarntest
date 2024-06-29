d93ae8499315fad55913cdf04528af3cf9ce80cc
Anthony <anthony@Apricot.local>
2024-06-29 14:31:18 +0200

    first

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import ReactDOM from "react-dom/client";
+import Numbers from './numbers';
+
+const App = () => {
+
+  return (
+    <Numbers initValue={42} />
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(<App />);
